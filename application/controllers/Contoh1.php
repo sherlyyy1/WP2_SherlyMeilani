e21ccf205d0d3752fdfdc0e88fbc976635a0c4be
sherlyyy1 <sherlymeilani00@gmail.com>
2023-11-06 23:51:01 -0700

    menambahkan tugas pertemuan 1 (installasi Codeigniter dan Contoh 1)

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Sherly Meilani
+        saya tinggal di Bekasi
+        olahraga yang saya sukai adalah Lari";
+    }
+}

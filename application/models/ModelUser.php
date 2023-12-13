18ab07c7de244fdb3cf18d2778057f6816ed0ab1
sherlyyy1 <sherlymeilani00@gmail.com>
2023-12-13 13:08:02 +0700

    menambahkan tugas pertemuan 12 (form tambah buku, tampilan data buku, dan model buku)

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -15,7 +15,7 @@ class ModelUser extends CI_Model
         return $this->db->get_where('user', $where);
     }
     public function cekUserAccess($where = null)
-    {  
+    {
         $this->db->select('*');
         $this->db->from('access_menu');
         $this->db->where($where);
